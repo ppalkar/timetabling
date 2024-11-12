4cfe1327916e69e17ef6375f69a2618c81deaeb0
RE-NY <rksamadkhani@gmail.com>
2024-11-12 23:30:15 +0530

    added gantt chart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 from amplpy import AMPL, add_to_path, variable
 
 # Initiailize the AMPL enviorment
@@ -41,6 +42,20 @@ def get_pd_df(x_var : variable.Variable) -> pd.DataFrame :
 def rename_columns(x_df : pd.DataFrame) -> pd.DataFrame :
     x_df.columns = ['TEAMS', 'TIMESLOTS', 'x']
     return x_df
+
+
+
+def parse_teams_per_prof(dat_file_path : str) -> dict :
+    # Parse Teams_per_prof set from .dat file
+    teams_per_prof = {}
+    with open(dat_file_path, 'r') as f:
+        content = f.read()
+        matches = re.findall(r"set Teams_per_prof\['(p\d+)'\] := ((?:\d+\s?)*)\;", content)
+        for prof, teams in matches:
+            teams_per_prof[prof] = list(map(int, teams.split()))
+    return teams_per_prof
+
+
 
 
 def make_gantt_chart(output_path : str, timeLength : int, teams_per_prof : dict) : 
@@ -102,29 +117,7 @@ x_df = rename_columns(x_df)
 # Saving new csv file
 x_df.to_csv(output_path, index = False)
 
-teams_per_prof = {
-    'p1': [3, 8, 16],
-    'p2': [3, 5, 16, 18, 21],
-    'p3': [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27],
-    'p4': [3, 15, 19, 20],
-    'p5': [12, 14, 20],
-    'p6': [15, 19],
-    'p7': [8, 9],
-    'p8': [9, 17, 18, 22, 24, 27],
-    'p9': [4, 10, 17, 24],
-    'p10': [10, 22],
-    'p11': [25],
-    'p12': [2, 25, 26, 27],
-    'p13': [26],
-    'p14': [4, 7],
-    'p15': [7, 12],
-    'p16': [5, 11],
-    'p17': [11],
-    'p19': [2, 21],
-    'p20': [6],
-    'p21': [6],
-    'p22': [14]
-}
+teams_per_prof = parse_teams_per_prof(data_path)
 
 # making gantt chart assuming time interval is 15min
 make_gantt_chart(output_path, 15, teams_per_prof)
